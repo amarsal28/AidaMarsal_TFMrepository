8a369ddf92033c954c74bc881e21f882ac0a288b
amarsal28 <amarsal@recerca.clinic.cat>
2024-12-05 12:45:20 +0100

    Add files via upload

--- 2_QualityControl.sh
+++ 2_QualityControl.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Control de qualitat amb NanoPlot
-
 # Descarreguem el paquet
 if ! pip show NanoPlot > /dev/null 2>&1; then
     pip install NanoPlot
@@ -17,29 +16,23 @@ do
     mkdir -p QC_Results/NanoPlot_QC/"$fastq_name"
 
     # Executem NanoPlot
-    NanoPlot --fastq "$fastq" -o QC_Results/NanoPlot_QC/"$fastq_name" -t 4 --title "$fastq Report" --plots kde dot
+    NanoPlot --fastq "$fastq" -o QC_Results/NanoPlot_QC/"$fastq_name" -t 4 --title "$fastq_name Report" --prefix "$fastq_name" --plots kde dot
 done
 
 
-# Control de qualitat amb NanoQC
-
-# Descarreguem el paquet NanoQC
-if ! pip show nanoQC > /dev/null 2>&1; then
-    pip install nanoQC
+# Control de qualitat amb FastQC
+# Descarreguem el paquet
+if ! command -v fastqc &> /dev/null
+then
+    sudo apt update
+    sudo apt install -y fastqc
 fi
 
 # Executem el codi
-for fastq in mice_fastq/*.fastq.gz;
-do
-    # Definim el nom de l'arxiu sense extensió
-    fastq_name=$(basename "$fastq" .fastq.gz)
+mkdir -p QC_Results/FastQC_results
+fastqc -o QC_Results/FastQC_results mice_fastq/*.fastq.gz
 
-    # Creem una carpeta específica per cada mostra
-    mkdir -p QC_Results/nanoQC_results/"$fastq_name"
 
-    # Executem NanoPlot
-    nanoQC -o QC_Results/nanoQC_results/"$fastq_name" "$fastq"
-done
 
 # Generació de document multiQC
-multiqc ./QC_Results
+multiqc ./QC_Results/ --filename multiQC_report --outdir QC_Results
