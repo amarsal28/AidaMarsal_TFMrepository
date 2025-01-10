8af4f0a53fc18882da51b539b66398e73a3f1292
amarsal28 <amarsal@recerca.clinic.cat>
2025-01-10 09:50:09 +0100

    Add files via upload

--- 7_QIIME2_taxonomyclass.sh
+++ 7_QIIME2_taxonomyclass.sh
@@ -3,42 +3,29 @@
 # Per la classificació farem servir el mètode classify-sklearn, i usem un arxiu "entrenat" a partir d'un arxiu de taxonomia i de seqüències de referència. 
 mkdir qiime2/tax_class
 qiime feature-classifier classify-sklearn \
-    --i-classifier silva_data/silva-138-99-nb-classifier.qza \
+    --i-classifier silva_data/silva_classifier.qza \
     --i-reads qiime2/clustered/rep-seqs-or-90.qza \
-    --o-classification qiime2/tax_class/table_tax_sklearn.qza \
-    --p-n-jobs 4
+    --o-classification qiime2/tax_class/table_tax_sklearn.qza 
 
-
-# També podem filtrar les dades per les taxes - exclourem cloroplast i mitocondria
+# També podem filtrar les dades per taxons - exclourem cloroplast, mitocondri, eucariotes i NA.
 qiime taxa filter-table \
   --i-table qiime2/clustered/table-or-90.qza \
   --i-taxonomy qiime2/tax_class/table_tax_sklearn.qza \
   --p-mode contains \
-  --p-exclude 'Chloroplast,Mitochondria' \
+  --p-exclude 'Chloroplast,Mitochondria,NA,d__Eukaryota' \
   --o-filtered-table qiime2/tax_class/table_tax_sklearn_filtered.qza
 
-# Com hem tret alguns ASVs de la taula, també ho podem filtrar per les seqüències
-qiime feature-table filter-seqs \
-  --i-data qiime2/clustered/rep-seqs-or-90.qza \
-  --i-table qiime2/tax_class/table_tax_sklearn_filtered.qza \
-  --o-filtered-data qiime2/clustered/rep-seqs-filtered.qza
-
-# Resumim els resultats
-qiime metadata tabulate \
-    --m-input-file qiime2/tax_class/table_tax_sklearn_filtered.qza \
+qiime feature-table summarize \
+    --i-table qiime2/tax_class/table_tax_sklearn_filtered.qza \
     --o-visualization qiime2/tax_class/table_tax_sklearn_filtered.qzv
-
-qiime tools export \
-    --input-path qiime2/tax_class/table_tax_sklearn_filtered.qzv \
-    --output-path qiime2/tax_class/table_tax_sklearn
 
 # Generem un gràfic interactiu per visualitzar les abundàncies de cada taxa, en cada mostra, mitjançant barres apilades
 qiime taxa barplot \
     --i-table qiime2/clustered/table-or-90.qza \
     --i-taxonomy qiime2/tax_class/table_tax_sklearn_filtered.qza \
-    --m-metadata-file mice_metadata.tsv \
-    --o-visualization qiime2/tax_class/taxa-barplots-sklearn-filtered.qzv
+    --m-metadata-file mice_data/mice_metadata.tsv \
+    --o-visualization qiime2/tax_class/taxa-barplots-sklearn.qzv
 
 qiime tools export \
-    --input-path qiime2/tax_class/taxa-barplots-sklearn-filtered.qzv \
+    --input-path qiime2/tax_class/taxa-barplots-sklearn.qzv \
     --output-path qiime2/tax_class/taxa-barplots-sklearn
--- 8_QIIME2_Diversity.sh
+++ 8_QIIME2_Diversity.sh
@@ -4,67 +4,14 @@
 mkdir qiime2/alignment
 
 qiime phylogeny align-to-tree-mafft-fasttree \
-    --p-n-threads 4 --p-parttree\
     --i-sequences qiime2/clustered/rep-seqs-or-90.qza \
     --o-alignment qiime2/alignment/aligned-rep-seqs-or-90.qza \
     --o-masked-alignment qiime2/alignment/masked-aligned-rep-seqs-or-90.qza \
     --o-tree qiime2/alignment/unrooted-tree.qza \
-    --o-rooted-tree qiime2/alignment/rooted-tree.qza
-    
-
-qiime tools export \
-    --input-path qiime2/alignment/rooted-tree.qza \
-    --output-path qiime2/alignment/rooted-tree
+    --o-rooted-tree qiime2/alignment/rooted-tree.qza \
+    --p-n-threads 24
 
 
-# Diversity core-metrics
-mkdir qiime2/diversity
-qiime diversity core-metrics-phylogenetic \
-    --i-phylogeny qiime2/alignment/rooted-tree.qza \
-    --i-table qimme2/clustered/table-or-90.qza \
-    --p-sampling-depth 15000 \
-    --m-metadata mice_metadata.tsv \
-    --output-dir qiime2/diversity/core-metrics-results
-
-# Associació de metadata i alfa-diversitat amb shannon-index
-qiime diversity alpha-group-significance \
-    --i-alpha-diversity qiime2/diversity/core-metrics-results/shannon_vector.qza \
-    --m-metadata-file mice_metadata.tsv \
-    --o-visualization qiime2/diversity/shannon-group-significance.qzv
 
 
-# Significance per variables categòriques. 
-# Per variables continues hauriem de fer servir correlation, però en el nostre cas no tenim.
-
-# beta-diversitat - diferències entre grups - genotip, tumor, familia
-qiime diversity beta-group-significance \
-    --i-distance-matrix qiime2/diversity/unweighted_unifrac_distance_matrix.qza \
-    --m-metadata-file mice_metadata.tsv \
-    --m-metadata-column genotype \
-    --o-visualization qiime2/diversity/beta-group-genotype.qzv
-
-
-qiime diversity beta-group-significance \
-    --i-distance-matrix qiime2/diversity/unweighted_unifrac_distance_matrix.qza \
-    --m-metadata-file mice_metadata.tsv \
-    --m-metadata-column family \
-    --o-visualization qiime2/diversity/beta-group-family.qzv
- 
-
-qiime diversity beta-group-significance \
-    --i-distance-matrix qiime2/diversity/unweighted_unifrac_distance_matrix.qza \
-    --m-metadata-file mice_metadata.tsv \
-    --m-metadata-column "HCC (YES/NO)" \
-    --o-visualization qiime2/diversity/beta-group-genotype.qzv
-
-
-# Alpha diversitat i rarefacció [p-max-depth depen de la mostra amb menys lectures]
-
-qiime diversity alpha-rarefaction \
-    --i-phylogeny qiime2/alignment/rooted-tree.qza \
-    --i-table qimme2/clustered/table-or-90.qza \
-    --p-max-depth 12500 \
-    --p-metrics shannon faith_pd\
-    --m-metadata-file mice_metadata.tsv \
-    --o-visualization qiime/diversity/alpha-rarefaction.qzv
 
